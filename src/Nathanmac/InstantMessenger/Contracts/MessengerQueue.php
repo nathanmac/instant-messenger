5ddae701fca41eeb860fbfef3a81a423c73b4587
Nathan Macnamara <nathan.macnamara@outlook.com>
2015-02-09 17:31:10 +0100

    Update queue interface for Messenger, missing queue name setting

--- src/Nathanmac/InstantMessenger/Contracts/MessengerQueue.php
+++ src/Nathanmac/InstantMessenger/Contracts/MessengerQueue.php
@@ -5,20 +5,19 @@ interface MessengerQueue {
     /**
      * Queue a new instance message for sending.
      *
-     * @param  array   $data
      * @param  \Closure|string  $callback
+     * @param  string  $queue
      * @return mixed
      */
-    public function queue(array $data, $callback);
+    public function queue($callback, $queue = null);
 
     /**
      * Queue a new instance message for sending after (n) seconds.
      *
      * @param  int  $delay
-     * @param  array  $data
      * @param  \Closure|string  $callback
+     * @param  string  $queue
      * @return mixed
      */
-    public function later($delay, array $data, $callback);
-
+    public function later($delay, $callback, $queue = null);
 }
