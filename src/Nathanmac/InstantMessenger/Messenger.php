20c0375ab8385fdb2d3df5a5e81e8ca441801402
Nathan Macnamara <nathan.macnamara@outlook.com>
2015-02-11 14:10:38 +0000

    Update messenger make queueable

--- src/Nathanmac/InstantMessenger/Messenger.php
+++ src/Nathanmac/InstantMessenger/Messenger.php
@@ -1,14 +1,18 @@
 <?php namespace Nathanmac\InstantMessenger;
 
 use Closure;
+use Illuminate\Contracts\Queue\Job;
 use Psr\Log\LoggerInterface;
+use SuperClosure\Serializer;
 use InvalidArgumentException;
+use Illuminate\Contracts\Queue\Queue;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Container\Container;
 use Nathanmac\InstantMessenger\Services\MessengerService;
 use Nathanmac\InstantMessenger\Contracts\Messenger as MessengerContract;
 use Nathanmac\InstantMessenger\Contracts\MessengerQueue as MessengerQueueContract;
 
-class Messenger implements MessengerContract {
+class Messenger implements MessengerContract, MessengerQueueContract {
 
     /**
      * The message service.
@@ -37,6 +41,13 @@ class Messenger implements MessengerContract {
      * @var \Illuminate\Contracts\Container\Container
      */
     protected $container;
+
+    /**
+     * The queue implementation.
+     *
+     * @var \Illuminate\Contracts\Queue\Queue
+     */
+    protected $queue;
 
     /**
      * Indicates if the actual sending is disabled.
@@ -73,6 +84,103 @@ class Messenger implements MessengerContract {
     }
 
     /**
+     * Queue a new instance message for sending.
+     *
+     * @param  \Closure|string $callback
+     * @param  string  $queue
+     * @return mixed
+     */
+    public function queue($callback, $queue = null)
+    {
+        $callback = $this->buildQueueCallable($callback);
+
+        return $this->queue->push('messenger@handleQueuedMessage', compact('callback'), $queue);
+    }
+
+    /**
+     * Queue a new message for sending on the given queue.
+     *
+     * @param  string  $queue
+     * @param  \Closure|string  $callback
+     * @return mixed
+     */
+    public function queueOn($queue, $callback)
+    {
+        return $this->queue($callback, $queue);
+    }
+
+    /**
+     * Queue a new message for sending after (n) seconds.
+     *
+     * @param  int  $delay
+     * @param  \Closure|string  $callback
+     * @param  string  $queue
+     * @return mixed
+     */
+    public function later($delay, $callback, $queue = null)
+    {
+        $callback = $this->buildQueueCallable($callback);
+
+        return $this->queue->later($delay, 'messenger@handleQueuedMessage', compact('callback'), $queue);
+    }
+
+    /**
+     * Queue a new message for sending after (n) seconds on the given queue.
+     *
+     * @param  string  $queue
+     * @param  int  $delay
+     * @param  \Closure|string  $callback
+     * @return mixed
+     */
+    public function laterOn($queue, $delay, $callback)
+    {
+        return $this->later($delay, $callback, $queue);
+    }
+
+    /**
+     * Build the callable for a queued message job.
+     *
+     * @param  mixed  $callback
+     * @return mixed
+     */
+    protected function buildQueueCallable($callback)
+    {
+        if ( ! $callback instanceof Closure) return $callback;
+
+        return (new Serializer)->serialize($callback);
+    }
+
+    /**
+     * Handle a queued message job.
+     *
+     * @param  \Illuminate\Contracts\Queue\Job  $job
+     * @param  array  $data
+     * @return void
+     */
+    public function handleQueuedMessage(Job $job, $data)
+    {
+        $this->send($this->getQueuedCallable($data));
+
+        $job->delete();
+    }
+
+    /**
+     * Get the true callable for a queued message.
+     *
+     * @param  array  $data
+     * @return mixed
+     */
+    protected function getQueuedCallable(array $data)
+    {
+        if (str_contains($data['callback'], 'SerializableClosure'))
+        {
+            return unserialize($data['callback'])->getClosure();
+        }
+
+        return $data['callback'];
+    }
+
+    /**
      * Call the provided message builder.
      *
      * @param  \Closure|string  $callback
@@ -86,6 +194,10 @@ class Messenger implements MessengerContract {
         if ($callback instanceof Closure)
         {
             return call_user_func($callback, $message);
+        }
+        elseif (is_string($callback))
+        {
+            return $this->container->make($callback)->messenger($message);
         }
 
         throw new InvalidArgumentException("Callback is not valid.");
@@ -159,4 +271,47 @@ class Messenger implements MessengerContract {
         return $this;
     }
 
+    /**
+     * Set the queue manager instance.
+     *
+     * @param  \Illuminate\Contracts\Queue\Queue  $queue
+     * @return $this
+     */
+    public function setQueue(Queue $queue)
+    {
+        $this->queue = $queue;
+
+        return $this;
+    }
+
+    /**
+     * Set the IoC container instance.
+     *
+     * @param  \Illuminate\Contracts\Container\Container  $container
+     * @return void
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Return the messenger service.
+     *
+     * @return MessengerService
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * Set the messenger service instance.
+     *
+     * @param MessengerService $service
+     */
+    public function setService(MessengerService $service)
+    {
+        $this->service = $service;
+    }
 }
