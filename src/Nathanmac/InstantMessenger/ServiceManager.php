0677761b0f6ab03575833d79f6cb51129281bc47
Nathan Macnamara <nathan.macnamara@outlook.com>
2015-02-20 19:45:38 +0000

    Driver builders for HipChat/Slack/Hall/Jaconda

--- src/Nathanmac/InstantMessenger/ServiceManager.php
+++ src/Nathanmac/InstantMessenger/ServiceManager.php
@@ -1,28 +1,42 @@
 <?php namespace Nathanmac\InstantMessenger;
 
 use Illuminate\Support\Manager;
+use Nathanmac\InstantMessenger\Services\HallService;
+use Nathanmac\InstantMessenger\Services\HipChatService;
+use Nathanmac\InstantMessenger\Services\JacondaService;
+use Nathanmac\InstantMessenger\Services\SlackService;
 
 class ServiceManager extends Manager {
 
-
     public function createHipchatDriver()
     {
+        $config = $this->app['config']['messenger.connections']['hipchat'];
+        $service = HipChatService::newInstance($config['token'], $config['room'], $config['notify']);
 
+        if (isset($config['color']))
+        {
+            $service->setColor($config['color']);
+        }
+
+        return $service;
     }
 
     public function createSlackDriver()
     {
-
+        $config = $this->app['config']['messenger.connections']['slack'];
+        return new SlackService($config['token'], $config['channel']);
     }
 
     public function createHallDriver()
     {
-
+        $config = $this->app['config']['messenger.connections']['hall'];
+        return new HallService($config['token']);
     }
 
     public function createJacondaDriver()
     {
-
+        $config = $this->app['config']['messenger.connections']['jaconda'];
+        return new JacondaService($config['account'], $config['token'], $config['room']);
     }
 
     /**
