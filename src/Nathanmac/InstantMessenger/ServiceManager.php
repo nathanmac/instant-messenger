bd31705e2375fc73bc5038f37c8906d3900c8190
Nathan Macnamara <nathan.macnamara@outlook.com>
2015-02-24 14:48:19 +0100

    Minior change to the configuration fetching.

--- src/Nathanmac/InstantMessenger/ServiceManager.php
+++ src/Nathanmac/InstantMessenger/ServiceManager.php
@@ -11,7 +11,7 @@ class ServiceManager extends Manager {
 
     public function createHipchatDriver()
     {
-        $config = $this->app['config']['messenger.connections']['hipchat'];
+        $config = $this->app['config']['messenger.connections.hipchat'];
         $service = HipChatService::newInstance($config['token'], $config['room'], $config['notify']);
 
         if (isset($config['color']))
@@ -24,25 +24,25 @@ class ServiceManager extends Manager {
 
     public function createSlackDriver()
     {
-        $config = $this->app['config']['messenger.connections']['slack'];
+        $config = $this->app['config']['messenger.connections.slack'];
         return SlackService::newInstance($config['token'], $config['channel']);
     }
 
     public function createHallDriver()
     {
-        $config = $this->app['config']['messenger.connections']['hall'];
+        $config = $this->app['config']['messenger.connections.hall'];
         return HallService::newInstance($config['token']);
     }
 
     public function createJacondaDriver()
     {
-        $config = $this->app['config']['messenger.connections']['jaconda'];
+        $config = $this->app['config']['messenger.connections.jaconda'];
         return JacondaService::newInstance($config['account'], $config['token'], $config['room']);
     }
 
     public function createSqwiggleDriver()
     {
-        $config = $this->app['config']['messenger.connections']['sqwiggle'];
+        $config = $this->app['config']['messenger.connections.sqwiggle'];
         return SqwiggleService::newInstance($config['token'], $config['stream']);
     }
 
